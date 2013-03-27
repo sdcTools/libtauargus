f247ef771376aa048fb717116fdeb1c9df88ad58
Robert de Vries <robert.de.vries@hinttech.com>
2013-03-27 16:00:00 +0100

    Added help source files.

---
+++ Help/Topic2Context.h
@@ -0,0 +1,59 @@
+
+// Library ID starts with 0000
+
+// HelpContexts may be taken from original IDL
+#define IDH_NEWTAUARGUSLib 51
+
+// Classes start with 1000
+#define IDH_TauArgCtrl 1001
+
+// Methods start with 3000
+#define IDH_ApplyRecode 3007
+#define IDH_CheckRealizedLowerAndUpperValues 3050
+#define IDH_CleanAll 3008
+#define IDH_CompletedTable 3045
+#define IDH_ComputeCodesToIndices 3051
+#define IDH_ComputeTables 3005
+#define IDH_DoActiveRecode 3019
+#define IDH_DoRecode 3006
+#define IDH_ExploreFile 3012
+#define IDH_GetCellDistance 3035
+#define IDH_GetCellStatusFreq 3033
+#define IDH_GetMaxnUc 3013
+#define IDH_GetStatusAndCostPerDim 3016
+#define IDH_GetTableCell 3022
+#define IDH_GetTableRow 3010
+#define IDH_GetTotalTabelSize 3039
+#define IDH_GetVarCode 3027
+#define IDH_GetVarCodeProperties 3029
+#define IDH_GetVarNumberOfCodes 3018
+#define IDH_PrepareCellDistance 3034
+#define IDH_PrepareHITAS 3024
+#define IDH_SetHierarchicalCodelist 3026
+#define IDH_SetHierarchicalDigits 3009
+#define IDH_SetInCodeList 3041
+#define IDH_SetInTable 3044
+#define IDH_SetNumberTab 3004
+#define IDH_SetNumberVar 3003
+#define IDH_SetRealizedLowerAndUpper 3049
+#define IDH_SetSecondaryGHMITER 3032
+#define IDH_SetSecondaryHITAS 3025
+#define IDH_SetSecondaryJJFORMAT 3038
+#define IDH_SetTable 3021
+#define IDH_SetTableCellStatus 3015
+#define IDH_SetTableSafety 3023
+#define IDH_SetTableSafetyInfo 3047
+#define IDH_SetTotalsInCodeList 3042
+#define IDH_SetVarCodeActive 3017
+#define IDH_SetVariable 3020
+#define IDH_SetVariableForTable 3046
+#define IDH_ThroughTable 3043
+#define IDH_UndoRecode 3014
+#define IDH_UndoSecondarySuppress 3048
+#define IDH_UnsafeVariable 3011
+#define IDH_UnsafeVariableCodes 3028
+#define IDH_WriteCSV 3036
+#define IDH_WriteCellRecords 3040
+#define IDH_WriteGHMITERDataCell 3031
+#define IDH_WriteGHMITERSteuer 3030
+#define IDH_WriteJJFormat 3037
---
+++ Help/Topic2HTML.h
@@ -0,0 +1,59 @@
+
+; Library ID starts with 0000
+
+; HelpContexts may be taken from original IDL
+IDH_NEWTAUARGUSLib = LNEWTAUARGUSLib.html   ;51
+
+; Classes start with 1000
+IDH_TauArgCtrl = CTauArgCtrl.html   ;1001
+
+; Methods start with 3000
+IDH_ApplyRecode = MApplyRecode.html   ;3007
+IDH_CheckRealizedLowerAndUpperValues = MCheckRealizedLowerAndUpperValues.html   ;3050
+IDH_CleanAll = MCleanAll.html   ;3008
+IDH_CompletedTable = MCompletedTable.html   ;3045
+IDH_ComputeCodesToIndices = MComputeCodesToIndices.html   ;3051
+IDH_ComputeTables = MComputeTables.html   ;3005
+IDH_DoActiveRecode = MDoActiveRecode.html   ;3019
+IDH_DoRecode = MDoRecode.html   ;3006
+IDH_ExploreFile = MExploreFile.html   ;3012
+IDH_GetCellDistance = MGetCellDistance.html   ;3035
+IDH_GetCellStatusFreq = MGetCellStatusFreq.html   ;3033
+IDH_GetMaxnUc = MGetMaxnUc.html   ;3013
+IDH_GetStatusAndCostPerDim = MGetStatusAndCostPerDim.html   ;3016
+IDH_GetTableCell = MGetTableCell.html   ;3022
+IDH_GetTableRow = MGetTableRow.html   ;3010
+IDH_GetTotalTabelSize = MGetTotalTabelSize.html   ;3039
+IDH_GetVarCode = MGetVarCode.html   ;3027
+IDH_GetVarCodeProperties = MGetVarCodeProperties.html   ;3029
+IDH_GetVarNumberOfCodes = MGetVarNumberOfCodes.html   ;3018
+IDH_PrepareCellDistance = MPrepareCellDistance.html   ;3034
+IDH_PrepareHITAS = MPrepareHITAS.html   ;3024
+IDH_SetHierarchicalCodelist = MSetHierarchicalCodelist.html   ;3026
+IDH_SetHierarchicalDigits = MSetHierarchicalDigits.html   ;3009
+IDH_SetInCodeList = MSetInCodeList.html   ;3041
+IDH_SetInTable = MSetInTable.html   ;3044
+IDH_SetNumberTab = MSetNumberTab.html   ;3004
+IDH_SetNumberVar = MSetNumberVar.html   ;3003
+IDH_SetRealizedLowerAndUpper = MSetRealizedLowerAndUpper.html   ;3049
+IDH_SetSecondaryGHMITER = MSetSecondaryGHMITER.html   ;3032
+IDH_SetSecondaryHITAS = MSetSecondaryHITAS.html   ;3025
+IDH_SetSecondaryJJFORMAT = MSetSecondaryJJFORMAT.html   ;3038
+IDH_SetTable = MSetTable.html   ;3021
+IDH_SetTableCellStatus = MSetTableCellStatus.html   ;3015
+IDH_SetTableSafety = MSetTableSafety.html   ;3023
+IDH_SetTableSafetyInfo = MSetTableSafetyInfo.html   ;3047
+IDH_SetTotalsInCodeList = MSetTotalsInCodeList.html   ;3042
+IDH_SetVarCodeActive = MSetVarCodeActive.html   ;3017
+IDH_SetVariable = MSetVariable.html   ;3020
+IDH_SetVariableForTable = MSetVariableForTable.html   ;3046
+IDH_ThroughTable = MThroughTable.html   ;3043
+IDH_UndoRecode = MUndoRecode.html   ;3014
+IDH_UndoSecondarySuppress = MUndoSecondarySuppress.html   ;3048
+IDH_UnsafeVariable = MUnsafeVariable.html   ;3011
+IDH_UnsafeVariableCodes = MUnsafeVariableCodes.html   ;3028
+IDH_WriteCSV = MWriteCSV.html   ;3036
+IDH_WriteCellRecords = MWriteCellRecords.html   ;3040
+IDH_WriteGHMITERDataCell = MWriteGHMITERDataCell.html   ;3031
+IDH_WriteGHMITERSteuer = MWriteGHMITERSteuer.html   ;3030
+IDH_WriteJJFormat = MWriteJJFormat.html   ;3037
